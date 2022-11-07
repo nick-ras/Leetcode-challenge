a5f19669dd983538bf59d21725f8ed14f23a1bc6
nick-ras <nick-ras@mail.dk>
2022-11-07 14:34:12 +0100

    script that sends email if youtube.com changes

--- developit.py
+++ developit.py
@@ -3,26 +3,24 @@ from email.message import EmailMessage
 import ssl
 import smtplib
 
-class emails(id):
-	email_sender = 'ttestesen81@gmail.com'
-	email_password = 'skbghjqffvrsxejl' #os.environ.get("EMAIL") #FIX
-	email_reciever = "nick-ras@mail.dk"
+class emails():
 
-	em = EmailMessage()
-	subject = ''
-	body = """"""
+	def send_welcome_email(self):
+		email_sender = 'ttestesen81@gmail.com'
+		email_password = 'skbghjqffvrsxejl' #os.environ.get("EMAIL") #FIX
+		email_reciever = "nick-ras@mail.dk"
 
-	em['From'] = email_sender
-	em['To'] = email_reciever
-	em['Subject'] = subject
-	em.set_content(body)
-	context = ssl.create_default_context()
+		em = EmailMessage()
+		subject = ''
+		body = """"""
 
-	def send_welcome_email(self, em, email_sender, email_password, context, email_reciever):
+		em['From'] = email_sender
+		em['To'] = email_reciever
+		em['Subject'] = subject
+		em.set_content(body)
+		context = ssl.create_default_context()
 		subject = 'welcome'
 		body = """welcome to automation"""
 		with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
 			smtp.login(email_sender, email_password)
 			smtp.sendmail(email_sender, email_reciever, em.as_string())
-
-emails.
--- website_change.py
+++ website_change.py
@@ -2,38 +2,35 @@ from datetime import datetime
 import time
 import hashlib
 from urllib.request import urlopen, Request
-
+from developit import emails
 
 class website_check():
-
-	url = Request('https://youtube.com/',
-								headers={'User-Agent': 'Mozilla/5.0'})
-
-	# to perform a GET request and load the
-	# content of the website and store it in a var
-	response = urlopen(url).read()
-	currentHash = hashlib.sha224(response).hexdigest()
-	print("running")
-	time.sleep(10)
-	while True:
+	def check_changes(self):
+		url = Request('https://youtube.com/',
+									headers={'User-Agent': 'Mozilla/5.0'})
+		response = urlopen(url).read()
+		currentHash = hashlib.sha224(response).hexdigest()
+		print("running")
+		time.sleep(10)
+		while True:
 			try:
-					# perform the get request and store it in a var
+				# perform the get request and store it in a var
+				response = urlopen(url).read()
+				currentHash = hashlib.sha224(response).hexdigest()
+				time.sleep(30)
+				response = urlopen(url).read()
+				newHash = hashlib.sha224(response).hexdigest()
+				if newHash == currentHash:
+					continue
+				else:
+					emails().send_welcome_email()
 					response = urlopen(url).read()
 					currentHash = hashlib.sha224(response).hexdigest()
 					time.sleep(30)
-					response = urlopen(url).read()
-					newHash = hashlib.sha224(response).hexdigest()
-					if newHash == currentHash:
-							continue
-					else:
-							print("something changed")
-							response = urlopen(url).read()
-							currentHash = hashlib.sha224(response).hexdigest()
-							time.sleep(30)
-							continue
+					continue
 			except Exception as e:
 					print("error")
 
-hey = startit(3)
+check = website_check().check_changes()
 
-hey.time()
+check()
