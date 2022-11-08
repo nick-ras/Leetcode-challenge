804dc377888e55b8a3af2fa737c662f3ca99eda7
Nick R <Nick-ras@mail.dk>
2022-11-08 21:30:39 +0100

    working on recursion

--- unique_paths.py
+++ unique_paths.py
@@ -1,18 +1,23 @@
 class Solution():
+	# def __init__(self):
+	# 	moves_init = 0
 
 	def recursive(self, m, n, hori, vert, moves):
-		if (vert >= m or hori >= n):
-			return
-		moves = moves + 1
-		Solution.recursive(self, hori + 1, 2)
-		Solution.recursive(self, vert + 1, 2)
+		if (vert >= m and hori >= n):
+			print(f"hey{moves}")
+			return moves
+		print(moves)
+		if (hori < m):
+			Solution.recursive(self, m, n, hori + 1, vert, moves + 1)
+		if (vert < m):
+			Solution.recursive(self, m, n, hori, vert + 1, moves + 1)
 		
 
 	def uniquePaths(self, m, n):
 		hori = 0
 		vert = 0
-		moves = 0
-		Solution.recursive(self, m, n, hori, vert, moves)
+		moves = Solution.recursive(self, m, n, hori, vert, int(0))
+		print(moves)
 
 obj = Solution()
 obj.uniquePaths(int(2), int(2))
