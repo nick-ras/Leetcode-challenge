ba346425e00ee26f09458a8558ab80c3b592dfb4
Nick R <Nick-ras@mail.dk>
2022-11-09 11:23:26 +0100

    it works, and gives numbers as expected, but no matter what code i put into the leetcode tester, then it says time has exceeded

--- unique_paths.py
+++ unique_paths.py
@@ -1,19 +1,12 @@
-# git add .\unique_paths.py ; git commit -m "h" ; git push
+#git add .\unique_paths.py ; git commit -m "h" ; git push
 class Solution():
 
-	def recursive(self, m, n, hori, vert, moves):
-		moves + 1
-		if (vert >= m or hori >= n):
-			print(f"if {moves}")
-			return int(moves)
-		moves = 1 + Solution.recursive(self, m, n, hori + 1, vert, moves + 1) + Solution.recursive(self, m, n, hori, vert + 1, moves + 1)
-		
+	def recursive(self, m, n, hori, vert):
+		if (vert == m or hori == n):
+			return 1
+		return Solution.recursive(self, m, n, hori + 1, vert) + Solution.recursive(self, m, n, hori, vert + 1)
 
 	def uniquePaths(self, m, n):
-		hori = 1
-		vert = 1
-		res = Solution.recursive(self, m, n, hori, vert, int(0))
-		print(f"end   {res}")
+		res = Solution.recursive(self, m, n, 1, 1)
+		return res
 
-obj = Solution()
-obj.uniquePaths(int(3), int(7))
